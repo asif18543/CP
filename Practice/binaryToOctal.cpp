a84f9385bc43d33e0181ae9a77842c291a3fca85
Asif Iqubal <57530645+asif18543@users.noreply.github.com>
2021-08-30 17:48:44 +0530

    Binary to octal conversion

---
+++ Practice/binaryToOctal.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int binaryToOctal(int n){
+    int x=1;
+    int ans=0;
+    while(n>0){
+    int y= n%10;
+    ans= ans + x*y;
+    x=x*8;
+    n=n/10;
+    
+    }
+    return ans;
+}
+
+int main() {
+    
+    int n; cin>>n;
+    cout<<binaryToOctal(n)<<endl;
+
+    return 0;
+}
