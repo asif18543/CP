caad523ea1dcfedb93ade2bcbd1f95a04360ca23
Asif Iqubal <57530645+asif18543@users.noreply.github.com>
2021-08-30 17:21:35 +0530

    Finding Pythagarous Triplet

---
+++ Practice/PythagorousTriplet.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool check(int x, int y, int z){
+    int a = max(x , max(y,z));
+  int b,c;
+  if(a==x){
+  b=y; c=z;
+  }
+  else if(a==y){
+    b=x; c=z;
+  }
+  else{
+  b=x; c=y;
+  }
+  if(a*a== b*b + c*c){
+    return true;
+  }
+  return false;
+}
+int main(){
+int x,y,z; cin>>x>>y>>z;
+  cout<<check(x,y,z)<<endl;
+  }
