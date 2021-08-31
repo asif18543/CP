25cf338a45d67181b4712c59386fc03b6aa7d6f3
Asif Iqubal <57530645+asif18543@users.noreply.github.com>
2021-08-31 17:06:57 +0530

    binary Searching

---
+++ Practice/binarySearch.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(int arr[], int n, int k){
+    int s=0; int e=n;
+    while(s<=e){
+        int mid = (s+e)/2;
+        if(arr[mid]==k){
+            return mid;
+        }
+        else if(arr[mid]>k){
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+int main(){
+    int n; cin>>n;
+    
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+     int k; cin>>k;
+   
+    cout<<binarySearch(arr,n,k)<<endl;
+
+}
